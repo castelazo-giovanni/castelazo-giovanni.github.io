9e2fe050598afef39ef659b6c50349a7e8d22674
castelazo-giovanni <38674339+castelazo-giovanni@users.noreply.github.com>
2018-06-24 03:23:32 -0500

    franklin-9
    fixed <figurecaption> error

--- scripts/byuitowndata.js
+++ scripts/byuitowndata.js
@@ -4,17 +4,17 @@ townRequest.open("GET", apiURL, true);
 townRequest.send();
 townRequest.onload =  function () {
   let townData = JSON.parse(townRequest.responseText);
-    document.getElementById('franklinMotto').innerHTML = townData.towns[0].motto;
+    document.getElementById('franklinMotto').innerHTML = "<strong>" + townData.towns[0].motto + "</strong>";
     document.getElementById('franklinYearFounded').innerHTML = townData.towns[0].yearFounded;    
     document.getElementById('franklinPopulation').innerHTML = townData.towns[0].currentPopulation;
     document.getElementById('franklinAnnualRainfall').innerHTML = townData.towns[0].averageRainfall;
 
-    document.getElementById('greenvilleMotto').innerHTML = townData.towns[1].motto;
+    document.getElementById('greenvilleMotto').innerHTML = "<strong>" + townData.towns[1].motto + "</strong>";
     document.getElementById('greenvilleYearFounded').innerHTML = townData.towns[1].yearFounded;    
     document.getElementById('greenvillePopulation').innerHTML = townData.towns[1].currentPopulation;
     document.getElementById('greenvilleAnnualRainfall').innerHTML = townData.towns[1].averageRainfall;
 
-    document.getElementById('springfieldMotto').innerHTML = townData.towns[3].motto;
+    document.getElementById('springfieldMotto').innerHTML = "<strong>" + townData.towns[3].motto + "</strong>";
     document.getElementById('springfieldYearFounded').innerHTML = townData.towns[3].yearFounded;    
     document.getElementById('springfieldPopulation').innerHTML = townData.towns[3].currentPopulation;
     document.getElementById('springfieldAnnualRainfall').innerHTML = townData.towns[3].averageRainfall;
