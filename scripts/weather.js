d019684cf9f697957f741f2dc6eadd40a14b0a78
castelazo-giovanni <38674339+castelazo-giovanni@users.noreply.github.com>
2018-06-24 03:02:44 -0500

    index.html
    fixed the links and wind chill calculation

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,22 +1,20 @@
 let weatherRequest = new XMLHttpRequest();
-var apiURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4156210&units=imperial&APPID=c20a9a0bba06a3103f735c16364790f3";
 weatherRequest.open("GET", apiURL, true);
 weatherRequest.send();
 weatherRequest.onload =  function () {
   let weatherData = JSON.parse(weatherRequest.responseText);
-    document.getElementById('franklinMotto').innerHTML = weatherData.towns[0].motto;
-    document.getElementById('franklinYearFounded').innerHTML = weatherData.towns[0].yearFounded;    
-    document.getElementById('franklinPopulation').innerHTML = weatherData.towns[0].currentPopulation;
-    document.getElementById('franklinAnnualRainfall').innerHTML = weatherData.towns[0].averageRainfall;
-
-    document.getElementById('greenvilleMotto').innerHTML = weatherData.towns[1].motto;
-    document.getElementById('greenvilleYearFounded').innerHTML = weatherData.towns[1].yearFounded;    
-    document.getElementById('greenvillePopulation').innerHTML = weatherData.towns[1].currentPopulation;
-    document.getElementById('greenvilleAnnualRainfall').innerHTML = weatherData.towns[1].averageRainfall;
-
-    document.getElementById('springfieldMotto').innerHTML = weatherData.towns[3].motto;
-    document.getElementById('springfieldYearFounded').innerHTML = weatherData.towns[3].yearFounded;    
-    document.getElementById('springfieldPopulation').innerHTML = weatherData.towns[3].currentPopulation;
-    document.getElementById('springfieldAnnualRainfall').innerHTML = weatherData.towns[3].averageRainfall;
-
+    
+    var t = (weatherData.main.temp_max + weatherData.main.temp_min) / 2;
+    var s = weatherData.wind.speed;
+    var f = (35.75 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16))).toFixed(2);
+    //convertLow = convertLow.toFixed(2);
+    
+    document.getElementById('weatherIcon').innerHTML = "<img src=\'http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png' alt='Franklin City Center' width='150'><br>";
+    document.getElementById('conditions').innerHTML = "<strong>" + weatherData.weather[0].description + " </strong>";
+    document.getElementById('current-temp').innerHTML = "<strong>" + weatherData.main.temp + "&degF </strong>";    
+    document.getElementById('highTemp').innerHTML = "<strong>" + weatherData.main.temp_max + "&degF </strong>";
+    document.getElementById('lowTemp').innerHTML = "<strong>" + weatherData.main.temp_min + "&degF </strong>";
+    document.getElementById('windSpeed').innerHTML = "<strong>" + weatherData.wind.speed + " mph </strong>";
+    document.getElementById('windChill').innerHTML = "<strong>" + f + "&degF </strong>";
 }
