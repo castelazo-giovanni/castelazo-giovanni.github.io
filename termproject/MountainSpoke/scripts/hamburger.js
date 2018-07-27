85bf8fef2679db7c06130f754c22cff878782edc
castelazo-giovanni <38674339+castelazo-giovanni@users.noreply.github.com>
2018-07-27 11:10:57 -0500

    TermProject Move
    Moved termprojectfolder to same level as assignments as stated in announcement

---
+++ termproject/MountainSpoke/scripts/hamburger.js
@@ -0,0 +1,9 @@
+function openHamburger() {
+
+    document.getElementById("mainmenu").classList.toggle("navigation");
+}
+
+function joinList() {
+    alert("Thank you your interest!  Your information has been submitted");
+    document.getElementById('joinMailingList').reset();
+}
