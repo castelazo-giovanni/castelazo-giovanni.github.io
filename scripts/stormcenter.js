1adf0979ff1e0aa6465d4be44cf49cba447f3173
castelazo-giovanni <38674339+castelazo-giovanni@users.noreply.github.com>
2020-08-22 07:15:19 -0500

    Removed CIT230
    Removed all my CIT230 files

--- scripts/stormcenter.js
+++
@@ -1,9 +0,0 @@
-function adjustRating(rating) {
-    document.getElementById("ratingvalue").innerHTML = rating;
-}
-
-
-var theDate = document.querySelector('input[type="date"]');
-        theDate.addEventListener('change', function() {
-          console.log('blank date input:', theDate.value);
-        });
